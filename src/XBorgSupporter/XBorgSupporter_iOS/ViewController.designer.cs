d9e9c891a17ad720bfc2a6d2d64ec699fd5cbca6
Michael Görs <michael.goers@googlemail.com>
2016-02-05 04:41:39 +0100

    basic navigation added

--- src/XBorgSupporter/XBorgSupporter_iOS/ViewController.designer.cs
+++ src/XBorgSupporter/XBorgSupporter_iOS/ViewController.designer.cs
@@ -1,17 +1,61 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
 
 namespace XBorgSupporter_iOS
 {
 	[Register ("ViewController")]
 	partial class ViewController
 	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton btnProfile { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton btnSearch { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UINavigationItem Home { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField txtProfile { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField txtSearch { get; set; }
+
 		void ReleaseDesignerOutlets ()
 		{
+			if (btnProfile != null) {
+				btnProfile.Dispose ();
+				btnProfile = null;
+			}
+			if (btnSearch != null) {
+				btnSearch.Dispose ();
+				btnSearch = null;
+			}
+			if (Home != null) {
+				Home.Dispose ();
+				Home = null;
+			}
+			if (txtProfile != null) {
+				txtProfile.Dispose ();
+				txtProfile = null;
+			}
+			if (txtSearch != null) {
+				txtSearch.Dispose ();
+				txtSearch = null;
+			}
 		}
 	}
 }
